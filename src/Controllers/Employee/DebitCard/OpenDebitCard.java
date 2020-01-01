7ec52bb4cac6dcfac1160332eafe6d73987f06a5
Nilmani <nilmanikulaweera@gmail.com>
2020-01-01 09:00:21 +0530

    open debit card

--- src/Controllers/Employee/DebitCard/OpenDebitCard.java
+++ src/Controllers/Employee/DebitCard/OpenDebitCard.java
@@ -1,6 +1,11 @@
 package Controllers.Employee.DebitCard;
 
 import Controllers.Employee.EmployeeHome;
+import Helpers.Helpers;
+import Models.CurrentAccountModel;
+import Models.DebitCardModel;
+import Objects.CurrentAccount.CurrentAccount;
+import Validator.FormValidator;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
@@ -12,12 +17,15 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
 import java.time.LocalDate;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class OpenDebitCard {
 
     private EmployeeHome parent;
     private LocalDate todayDate;
     private LocalDate expirayDate;
+    private DebitCardModel debitModel;
 
     /*
     Card Number, Pin Number, Account Number, Issued Date Expiry Date
@@ -27,6 +35,7 @@ public class OpenDebitCard {
         this.parent = parent;
         this.todayDate =  LocalDate.now();
         this.expirayDate = todayDate.plusYears(4);
+        this.debitModel = new DebitCardModel();
     }
 
     public GridPane openDebitCardPane(BorderPane pane) {
@@ -82,10 +91,39 @@ public class OpenDebitCard {
         submitButton.setPrefSize(73, 35);
         pane1.add(submitButton, 3, 8);
         pane1.setHalignment(submitButton, HPos.CENTER);
+        submitButton.relocate(616,658);
 
         submitButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+
+                if(!savingsAccNumberText.getText().isEmpty()){
+
+                    long number = ThreadLocalRandom.current().nextLong(9999999999999999L);
+                    String card_number = String.valueOf(number);
+
+                    int pin =  new Random().nextInt(9999);
+                    String pin_number = String.valueOf(pin);
+                    //String card_number = "1111222255554444";
+                    //String pin_number = "4564";
+                    String result = debitModel.issueNewDebitCard(card_number,pin_number,savingsAccNumberText.getText(), LocalDate.now().toString(), expirayDate.toString());
+
+                    if(result.equals("Success")){
+                        Helpers.showAlert(Alert.AlertType.CONFIRMATION, pane.getScene().getWindow(), "Success!", "Issued new debit card");
+                        parent.enablePane();
+                        pane.getChildren().remove(pane1);
+                    }
+                    else{
+                        Helpers.showAlert(Alert.AlertType.ERROR, pane.getScene().getWindow(), "Form Error!", result);
+                        return;
+                    }
+
+                }
+                else {
+                    Helpers.showAlert(Alert.AlertType.ERROR, pane.getScene().getWindow(), "Form Error!", "Fill All the Fields");
+                    return;
+                }
+
             }
 
 
---
+++ src/Models/DebitCardModel.java
@@ -0,0 +1,34 @@
+package Models;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DebitCardModel {
+
+    private Connection connection;
+
+    public String issueNewDebitCard(String CardNumber, String PinNumber, String SavingsAccountNumber, String IssuedDate,
+                                    String ExpiryDate){
+        connection = DB.Database.getConnection();
+        String result = "Error! Try again.";
+        try {
+            String sql = "CALL `issueNewDebitCard`(?, ?, ?, ?, ?)";
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, CardNumber);
+            stmt.setString(2, PinNumber);
+            stmt.setString(3, SavingsAccountNumber);
+            stmt.setString(4, IssuedDate);
+            stmt.setString(5, ExpiryDate);
+
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                result = rs.getString(1);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
